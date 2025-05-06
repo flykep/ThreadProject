24d8957a8c5d669d5784689e1087547031a5c0e6
Aser <elsherbini.aser@itismeucci.com>
2025-05-06 11:23:40 +0200

    Esercizio svolto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nomi[] = new String[] { "Marco", "Diego", "Filippo", "Daniele" };
+        for (int i = 0; i < nomi.length; i++) {
+            new Corridore(nomi[i]).start();
+        }
+    }
+}
+
+class Corridore extends Thread {
+    private String nome;
+
+    public Corridore(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void run() {
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            count++;
+            System.out.println("nome : " + nome + " ---------> " + count + " metri");
+
+            if (count == 100) {
+                System.out.println("Il corridore " + nome + " e' arivato ");
+            }
+        }
     }
 }
